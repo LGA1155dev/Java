e48c419dcb68a803f1e0fa03c0ad7d6f7f81c67d
LGA1155dev <gabrielalveslga1155@gmail.com>
2026-04-12 12:39:22 -0300

    update to java 02
    sofrendo nessa linguagem... Pra que fui largar a linguagem C...

---
+++ Java-02/src/Java02.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Java02 {
+    public static void main (String[] args) {
+        Date data = new Date();
+        System.out.println("Hello wolrd");
+        System.out.println("Hoje é " + data);
+    }
+}
